44b2ec791574df1f0323b843471d8af88afbfbcb
Deepika M <deepikamohan18012001@gmail.com>
2025-01-30 19:45:28 +0530

    ME_BUILDOUT_QTIFY_V2_MODULE_ONE

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from "react";
 import styles from "./Button.module.css";
 
-const Button = ({children}) => {
-  return (
-    <button className={styles.button} >{children}</button>
-  )
+const Button=({children})=>{
+    return(
+        <button className={styles.button}>
+        {children}
+        </button>
+    )
 }
 
-export default Button
+export default Button;
